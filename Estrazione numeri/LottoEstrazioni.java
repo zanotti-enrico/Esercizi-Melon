b13c2f5ceef03519afa5b950b63ba4fdca77b136
Enrico Zanotti <enrico.zan07@gmail.com>
2024-01-28 20:59:42 +0100

    Finished LottoEstrazioni.java

--- Estrazione numeri/LottoEstrazioni.java
+++ Estrazione numeri/LottoEstrazioni.java
@@ -43,11 +43,30 @@ public class LottoEstrazioni {
                 case 2 : { //Giocata utente
                     System.out.println("Puntare i cinque numeri per la ruota\n");
 
+                    //Il valore inserito dall'utente all'interno del ciclo di for
+                    int valoreInserito;
+                    //Se il valore inserito dall'utente si trova gia' nell'array questa variabile viene impostata a falsa
+                    boolean continuareInserimento = true;
                     //Riempire l'array delle estrazioni dell'utente con i numeri richiesti
                     for (int estrazioneCorrente = 0; estrazioneCorrente < estrazioni; estrazioneCorrente++) {
-                        System.out.print("Estrazione " + estrazioneCorrente + ": ");
-                        //Salvare nell'array il numero inserito dall'utente
-                        estrazioniUtente[estrazioneCorrente] = keyboard.nextInt();
+                        do {
+                            System.out.print("Estrazione " + estrazioneCorrente + ": ");
+                            //Salvare nella variabile locale il valore inserito dall'utente
+                            valoreInserito = keyboard.nextInt();
+
+                            //Se il valore inserito si trova gia' nell'array allora stampare un messaggio di errore e non permettere un nuovo inserimento
+                            if(valorePresenteInArray(estrazioniUtente, valoreInserito, estrazioneCorrente))
+                            {
+                                System.out.println("Errore : i valori delle estrazioni non possono ripetersi.");
+                                //Impostando la variabile a falso il ciclo while si ripete
+                                continuareInserimento = false;
+                            //Se il valore inserito non si trova nell'array allora reimpostare la variabile per la terminazione dell'inserimento
+                            } else continuareInserimento = true;
+                        //Se un numero inserito non e' valido ripetere l'inserimento
+                        } while(!continuareInserimento);
+
+                        //Salvare il numero digitato nell'array una volta eseguito il controllo per assicurarsi che sia valido
+                        estrazioniUtente[estrazioneCorrente] = valoreInserito;
                     }
 
                     System.out.println("Estrazioni completate");
@@ -55,7 +74,21 @@ public class LottoEstrazioni {
                 }
 
                 case 3 : { //Verifica vincita
-                    
+                    //Contiene il numero di estrazioni dell'utente che sono presenti all'interno della ruota
+                    int estrazioniUtenteCorrette = nPresentiInArray(estrazioniRuota, estrazioniUtente);
+
+                    //In base alla quantita' di valori trovati presenti in entrambi gli array stampare una vittoria
+                    switch(estrazioniUtenteCorrette) {
+                        //Nel caso uno o nessun valore siano presenti in entrambe le ruote allora nessuna vincita
+                        case 0 : 
+                        case 1 : 
+                            System.out.println("Nessuna vincita."); break;
+
+                        case 2 : System.out.println("Vincita : terna"); break;
+                        case 3 : System.out.println("Vincita : quaterna");break;
+                        case 4 : System.out.println("Vincita : cinquina");break;
+                        case 5 : System.out.println("Vincita : quaterna");break;
+                    }
                 }
 
                 case 4 : { //Visualizza giocate
@@ -63,15 +96,13 @@ public class LottoEstrazioni {
 
                     System.out.println("Estrazioni ruota di Venezia: ");
                     //Visualizzare il contenuto degli array delle estrazioni
-                    for(int estrazioneCorrente = 0; estrazioneCorrente < estrazioni; estrazioneCorrente++) {
-                        System.out.println((estrazioneCorrente + 1) + ": " + estrazioniUtente[estrazioneCorrente]);
-                    }
+                    for(int estrazioneCorrente = 0; estrazioneCorrente < estrazioni; estrazioneCorrente++)
+                        System.out.println((estrazioneCorrente + 1) + ": " + estrazioniRuota[estrazioneCorrente]);
 
                     System.out.println("Estrazioni utente: ");
                     //Visualizzare il contenuto degli array delle estrazioni
-                    for(int estrazioneCorrente = 0; estrazioneCorrente < estrazioni; estrazioneCorrente++) {
+                    for(int estrazioneCorrente = 0; estrazioneCorrente < estrazioni; estrazioneCorrente++)
                         System.out.println((estrazioneCorrente + 1) + ": " + estrazioniUtente[estrazioneCorrente]);
-                    }
                 }
             }
         } while(selezione != 5);
@@ -166,6 +197,32 @@ public class LottoEstrazioni {
 
         //Ritornare l'output
         return output;
+    }
+
+    /*Dati due array di valori interi non ripetuti ritorna la quantita' di numeri che
+     * si trovano in entrambi gli array.
+     */
+    private static int nPresentiInArray(int[] primoArray, int[] secondoArray) {
+        //Conta gli elementi present in entrambi gli array
+        int contatoreElementiPresenti = 0;
+        //Viene impostata a vero quando il valore del primo array viene trovato nel secondo per saltare il resto della ricerca
+        boolean valoreUgualeTrovato;
+
+        //Iterare per ogni elemento del primo array alla ricerca di un elemento uguale nel secondo array
+        for(int ricercaPrimoArray = 0; ricercaPrimoArray < primoArray.length; ricercaPrimoArray++) {
+            valoreUgualeTrovato = false;
+
+            //Iterare nel secondo array fino a quando un valore uguale tra i due non viene trovato o l'array si termina
+            for(int ricercaSecondoArray = 0; ricercaSecondoArray < secondoArray.length && !valoreUgualeTrovato; ricercaSecondoArray++)
+                //Controllare se il valore corrente del primo e del secondo array sono uguali
+                if(primoArray[ricercaPrimoArray] == secondoArray[ricercaSecondoArray])
+                { //Se i due valori sono uguali incrementare il contatore e ricercare un nuovo valore
+                    contatoreElementiPresenti++;
+                    valoreUgualeTrovato = true;
+                }
+        }
+
+        return contatoreElementiPresenti;
     }
 
     /*Controlla se un valore si trova all'interno di un array 
